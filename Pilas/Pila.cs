c6ce04462d1c0d54d010d7d9d4c12ffa803b505a
Axel <UPNLCLAB1306>
2025-01-21 12:00:10 -0500

    avance 2

---
+++ Pilas/Pila.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pilas {
+    internal class Pila {
+        private Nodo cima = null;
+        private int contador = 0;   
+        public void push (string nom) {
+            Nodo nuevo = new Nodo();
+            nuevo.Nombre=nom;
+
+            nuevo.Siguiente=cima;
+            cima = nuevo;
+            contador++;
+        }
+        public void mostrar(ListBox list) {
+            Nodo actual = cima;
+            while (actual!=null) {
+               list.Items.Add(actual.Nombre);
+                actual = actual.Siguiente;
+            }
+        }
+    }
+}
